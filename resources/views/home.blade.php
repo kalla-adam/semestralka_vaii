b3d284abb765eb74c02014a597a3a34959c4cdd0
kalla-adam <adamkalla777@gmail.com>
2022-10-20 17:38:38 +0200

    indent fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,37 @@
 @extends('layouts.main')
 
 @section('title')
-    Home
+Home
 @endsection
 
 @section('content')
+<h1>
+    Home
+</h1>
 
-    <h1>
-        Home
-    </h1>
+<div class="navbar" id="Navbar">
+    <a href="{{route('home-page')}}" class="active">Home</a>
+    <a href="{{route('vrcholy-page')}}">Vrcholy</a>
+    <a href="{{route('krivan-page')}}">Kriváň</a>
+    <a href="{{route('dumbier-page')}}">Ďumbier</a>
+</div>
 
-    <div class="navbar" id="Navbar">
-        <a href="{{route('home-page')}}" class="active">Home</a>
-        <a href="{{route('vrcholy-page')}}">Vrcholy</a>
-        <a href="{{route('krivan-page')}}">Kriváň</a>
-        <a href="{{route('dumbier-page')}}">Ďumbier</a>
+<form action="#" method="post">
+    <div class="container">
+        <label><b>Prihlasovacie meno</b></label>
+        <input type="text" placeholder="Zadajte Prihlasovacie Meno" name="uname" required>
+    
+        <label><b>Heslo</b></label>
+        <input type="password" placeholder="Zadajte Heslo" name="psw" required>
+    
+        <button type="submit">Login</button>
+        <label>
+            <input type="checkbox" checked="checked" name="remember"> Zapamätaj si ma.
+        </label>
     </div>
-
-    <form action="#" method="post">
-        <div class="container">
-            <label><b>Prihlasovacie meno</b></label>
-            <input type="text" placeholder="Zadajte Prihlasovacie Meno" name="uname" required>
-      
-            <label><b>Heslo</b></label>
-            <input type="password" placeholder="Zadajte Heslo" name="psw" required>
-      
-            <button type="submit">Login</button>
-            <label>
-                <input type="checkbox" checked="checked" name="remember"> Zapamätaj si ma.
-            </label>
-        </div>
-      
-        <div class="container" style="background-color:#f0d0d0">
-            <span class="psw">Zabudli ste <a href="#">heslo?</a></span>
-        </div>
-      </form>
-
+    
+    <div class="container" style="background-color:#f0d0d0">
+        <span class="psw">Zabudli ste <a href="#">heslo?</a></span>
+    </div>
+</form>
 @endsection
--- resources/views/krivan.blade.php
+++ resources/views/krivan.blade.php
@@ -1,60 +1,58 @@
 @extends('layouts.main')
 
 @section('title')
-    Kriváň
+Kriváň
 @endsection
 
 @section('content')
+<h1>
+    Kriváň
+</h1>
 
-    <h1>
-        Kriváň
-    </h1>
+<div class="navbar" id="Navbar">
+    <a href="{{route('home-page')}}">Home</a>
+    <a href="{{route('vrcholy-page')}}">Vrcholy</a>
+    <a href="{{route('krivan-page')}}" class="active">Kriváň</a>
+    <a href="{{route('dumbier-page')}}">Ďumbier</a>
+</div>
 
-    <div class="navbar" id="Navbar">
-        <a href="{{route('home-page')}}">Home</a>
-        <a href="{{route('vrcholy-page')}}">Vrcholy</a>
-        <a href="{{route('krivan-page')}}" class="active">Kriváň</a>
-        <a href="{{route('dumbier-page')}}">Ďumbier</a>
-    </div>
+<div class="row">
+    <img src="storage/pics/krivan.jpg" alt="Krivan" class="column">
 
-    <div class="row">
-        <img src="storage/pics/krivan.jpg" alt="Krivan" class="column">
+    <p class="classic column">
+        Kriváň (poľ. Krywań, nem. Kriwan, maď. Kriván) je vrch na konci rázsochy, ktorá sa od hlavného hrebeňa Vysokých Tatier odvetvuje v Čubrine. 
+        Leží nad Nefcerkou, Kôprovou a Važeckou dolinou na svojimi J a JZ výbežkami zasahuje do Liptovskej kotliny. Je to jeden z najvyšších vrchov Slovenska. 
+        Dosahuje výšku 2 494,7 m n. m. Od 19. storočia je neoficiálnym symbolom slobody Slovákov a slovanskej súdržnosti.
+        Medzi rokmi 1960 až 1990 bol zobrazený s vatrou na znaku Slovenska v štátnom znaku Česko-Slovenska. Dočasne nahradil tradičný slovenský znak s trojvrším a krížom, 
+        ktorý je dnes v štátnej symbolike Slovenskej republiky. V roku 2005 bolo rozhodnuté, že bude zobrazený aj na slovenských eurominciach. 
+        Po Kriváni je pomenovaná aj planétka (24260) Kriváň.
+    </p>
+</div>
 
-        <p class="classic column">
-            Kriváň (poľ. Krywań, nem. Kriwan, maď. Kriván) je vrch na konci rázsochy, ktorá sa od hlavného hrebeňa Vysokých Tatier odvetvuje v Čubrine. 
-            Leží nad Nefcerkou, Kôprovou a Važeckou dolinou na svojimi J a JZ výbežkami zasahuje do Liptovskej kotliny. Je to jeden z najvyšších vrchov Slovenska. 
-            Dosahuje výšku 2 494,7 m n. m. Od 19. storočia je neoficiálnym symbolom slobody Slovákov a slovanskej súdržnosti.
-            Medzi rokmi 1960 až 1990 bol zobrazený s vatrou na znaku Slovenska v štátnom znaku Česko-Slovenska. Dočasne nahradil tradičný slovenský znak s trojvrším a krížom, 
-            ktorý je dnes v štátnej symbolike Slovenskej republiky. V roku 2005 bolo rozhodnuté, že bude zobrazený aj na slovenských eurominciach. 
-            Po Kriváni je pomenovaná aj planétka (24260) Kriváň.
-        </p>
-    </div>
+<div class="row">
+    <p class="classic column">
+        Krivánska časť rozsiahlych pozemkov, ktoré patrili do Liptova bola, po ústupe Tatárov (1241 – 1242), kráľovskými darovacími listinami pridelená zaslúžilým bojovníkom, ktorí ich prenajímali donátorom, šoltýsom, ktorí sa mali postarať o zaľudnenie a zúrodnenie kraja. Roku 1267 udelil Belo IV. comesovi Bogomelovi zalesnenú časť medzi Važcom a hranicami Spiša, rátajúc do toho liptovskú časť Vysokých Tatier s výnimkou Tichej a Kôprovej doliny, ktoré zostali korunným majetkom. Už predtým na tomto území žili osadníci, ktorí obrábali malé polia a chovali dobytok. Kolonizačný proces na tomto území sa pravdepodobne skončil v 13. storočí. V 15. – 16. storočí posilnila jestvujúce osídlenie valašská kolonizácia.[2] V druhej polovici 16. storočia v súvislosti s rozvojom baníctva v Boci sa rozvinulo v susedných obciach – Hybiach, Východnej a v Liptovskej Kokave – baníctvo. Turecké nebezpečenstvo, politická nespokojnosť uhorskej strednej šľachty, nedostatok finančných prostriedkov na udržiavanie vojsk podnietili vládnucich Habsburgovcov, aby hľadali chýbajúce prostriedky v ťažbe rúd a zlata. Cisár Ferdinand I., s pomocou štátneho dozoru, sa usiloval rozvinúť baníctvo v hornom Liptove. Územie Kriváňa pričlenili k správe komorských majetkov v Liptovskom Hrádku a do Tatier povolali odborníkov z okolia Banskej Bystrice, ktorí sa zaujímali o ložiská zlata nielen na Kriváni, ale aj vo všetkých susedných dolinách.
+    </p>
 
-    <div class="row">
-        <p class="classic column">
-            Krivánska časť rozsiahlych pozemkov, ktoré patrili do Liptova bola, po ústupe Tatárov (1241 – 1242), kráľovskými darovacími listinami pridelená zaslúžilým bojovníkom, ktorí ich prenajímali donátorom, šoltýsom, ktorí sa mali postarať o zaľudnenie a zúrodnenie kraja. Roku 1267 udelil Belo IV. comesovi Bogomelovi zalesnenú časť medzi Važcom a hranicami Spiša, rátajúc do toho liptovskú časť Vysokých Tatier s výnimkou Tichej a Kôprovej doliny, ktoré zostali korunným majetkom. Už predtým na tomto území žili osadníci, ktorí obrábali malé polia a chovali dobytok. Kolonizačný proces na tomto území sa pravdepodobne skončil v 13. storočí. V 15. – 16. storočí posilnila jestvujúce osídlenie valašská kolonizácia.[2] V druhej polovici 16. storočia v súvislosti s rozvojom baníctva v Boci sa rozvinulo v susedných obciach – Hybiach, Východnej a v Liptovskej Kokave – baníctvo. Turecké nebezpečenstvo, politická nespokojnosť uhorskej strednej šľachty, nedostatok finančných prostriedkov na udržiavanie vojsk podnietili vládnucich Habsburgovcov, aby hľadali chýbajúce prostriedky v ťažbe rúd a zlata. Cisár Ferdinand I., s pomocou štátneho dozoru, sa usiloval rozvinúť baníctvo v hornom Liptove. Územie Kriváňa pričlenili k správe komorských majetkov v Liptovskom Hrádku a do Tatier povolali odborníkov z okolia Banskej Bystrice, ktorí sa zaujímali o ložiská zlata nielen na Kriváni, ale aj vo všetkých susedných dolinách.
-        </p>
+    <img src="storage/pics/krivan1.jpg" alt="Krivan" class="column">
+</div>
 
-        <img src="storage/pics/krivan1.jpg" alt="Krivan" class="column">
-    </div>
+<div class="row">
+    <img src="storage/pics/krivan2.jpg" alt="Krivan" class="column">
 
-    <div class="row">
-        <img src="storage/pics/krivan2.jpg" alt="Krivan" class="column">
+    <p class="classic column">
+        Generácia štúrovcov, ktorá, v 19. storočí začala vystupovať proti národnému útlaku a presadzovala myšlienku slovanskej vzájomnosti. Dňa 24. septembra 1835 vystúpil na Kriváň so šiestimi druhmi mikulášsky knihár, nakladateľ, spisovateľ a osvetár, národný buditeľ Gašpar Fejérpataky-Belopotocký. O dva roky neskôr vyšiel v Kuzmányho Hronke opis jeho Cesty na Kriváň Liptowský, ktorý predstavuje prvú literárnu zmienku o chýrnom končiari v slovenskej literárnej tvorbe. Jeho výstup možno považovať za vzor a priamu pohnútku neskorších národných vychádzok, výstupov, pútí na Kriváň – symbolickú horu slovanskej jednoty a sily Slovákov.
+        Prvá z nich sa uskutočnila 16. augusta 1841. Vtedy spolu s Fejérpataky-Belopotockým vystúpili na vrchol dejatelia a politici tých čias Ľudovít Štúr a Jozef Miloslav Hurban. Vtedy s nimi vystúpil aj grécky knieža Ján Aristarchos i niekoľko žien. [7]
+        V Liptovskom Mikuláši sídlil miestny kňaz Michal Miloslav Hodža, ktorý nielen organizoval podujatia na vytvorenie spisovného jazyka a národné vystúpenie so Žiadosťami slovenského národa, ale sám sa podieľal na teoretickom zdôvodnení spisovnej slovenčiny a tiež na prípravách nadšených národných ciest na Kriváň. Na nich sa zúčastnil aj tretí zo slávnostného štúrovského súhvezdia Jozef Miloslav Hurban, ktorý 29. augusta 1844 vystúpil tiež s Jankom Kráľom, Jánom Franciscim a lekárom v Hybiach J. B. Guothom, rodákom z Liptova (Ľubela), na Kriváň a v revolučnom roku viedol do Liptova svojich povstaleckých hurbanistov. S Liptovom a Kriváňom bol teda úzko spojený celý uvedomovací a slovenčiaci pohyb štúrovcov.[8]
+    </p>
+</div>
 
-        <p class="classic column">
-            Generácia štúrovcov, ktorá, v 19. storočí začala vystupovať proti národnému útlaku a presadzovala myšlienku slovanskej vzájomnosti. Dňa 24. septembra 1835 vystúpil na Kriváň so šiestimi druhmi mikulášsky knihár, nakladateľ, spisovateľ a osvetár, národný buditeľ Gašpar Fejérpataky-Belopotocký. O dva roky neskôr vyšiel v Kuzmányho Hronke opis jeho Cesty na Kriváň Liptowský, ktorý predstavuje prvú literárnu zmienku o chýrnom končiari v slovenskej literárnej tvorbe. Jeho výstup možno považovať za vzor a priamu pohnútku neskorších národných vychádzok, výstupov, pútí na Kriváň – symbolickú horu slovanskej jednoty a sily Slovákov.
-            Prvá z nich sa uskutočnila 16. augusta 1841. Vtedy spolu s Fejérpataky-Belopotockým vystúpili na vrchol dejatelia a politici tých čias Ľudovít Štúr a Jozef Miloslav Hurban. Vtedy s nimi vystúpil aj grécky knieža Ján Aristarchos i niekoľko žien. [7]
-            V Liptovskom Mikuláši sídlil miestny kňaz Michal Miloslav Hodža, ktorý nielen organizoval podujatia na vytvorenie spisovného jazyka a národné vystúpenie so Žiadosťami slovenského národa, ale sám sa podieľal na teoretickom zdôvodnení spisovnej slovenčiny a tiež na prípravách nadšených národných ciest na Kriváň. Na nich sa zúčastnil aj tretí zo slávnostného štúrovského súhvezdia Jozef Miloslav Hurban, ktorý 29. augusta 1844 vystúpil tiež s Jankom Kráľom, Jánom Franciscim a lekárom v Hybiach J. B. Guothom, rodákom z Liptova (Ľubela), na Kriváň a v revolučnom roku viedol do Liptova svojich povstaleckých hurbanistov. S Liptovom a Kriváňom bol teda úzko spojený celý uvedomovací a slovenčiaci pohyb štúrovcov.[8]
-        </p>
-    </div>
+<div class="row">
+    <img src="storage/pics/krivan3.jpg" alt="Krivan" class="full-column">
 
-    <div class="row">
-        <img src="storage/pics/krivan3.jpg" alt="Krivan" class="full-column">
-
-        <p class="classic column">
-            Na Kriváni nie je núdza o záchranné akcie Horskej záchrannej služby i Vrtuľníkovej záchrannej zdravotnej služby. Piloti vrtuľníkov nemajú veľmi radi záchranné akcie v tejto oblasti. Posádky sa môžu dostať do kritických situácií, ktoré sú veľmi ťažko riešiteľné. Počas jedného leteckého výcviku, bolo to 17. augusta 1983, málo chýbalo, aby sa stala nehoda. Záchranári v masíve Kriváňa nacvičovali čo najrýchlejšie opustenie vrtuľníka v exponovanom teréne. Kriváň je pre pilotov nebezpečný štít. Každú sekundu musia byť v strehu, lebo na západnej strane Tatier, kde stojí tento ospevovaný štít, vejú zradné vetry zo všetkých strán, na helikoptéry tu číha termika i vzdušné diery.
-            Teda, v ten pekný jasný deň posádka vrtuľníka Mil Mi-8 pri vypúšťaní záchranára Martina Mladoňa s pomocou palubného navijaka na vrchol Kriváňa sa stroj znenazdajky, bez varovania, prepadol do doliny. Martin už bol na svojom stanovišti, stál na zemi, ešte sa nestihol odopnúť od lanka, keď prudký nečakaný pád vrtuľníka ho vytrhol aj s lankom do priepasti. Pilot zareagoval, odklonil stroj, natočil ho a pomaly ustálil. Len domyslieť si všetci mohli aké kruté chvíle pod vrtuľníkom prežíval Martin. Na tenkom lanku letel raz na jednu a potom na druhú stranu, ako na obrovskej hojdačke. Bol to okamih, na ktorý nikdy nezabudne. Ani potom, keď piloti lietali na nových moderných strojoch nevyhli sa nebezpečným nástrahám, ktoré pre nich pripravoval Kriváň. Prepadávali sa vo vzdušných dierach, prekonávali nebezpečné prúdenie vzduchu s nebývalým rešpektom lietajú okolo jeho stien aj teraz.[17]
-        </p>
-    </div>
-
+    <p class="classic column">
+        Na Kriváni nie je núdza o záchranné akcie Horskej záchrannej služby i Vrtuľníkovej záchrannej zdravotnej služby. Piloti vrtuľníkov nemajú veľmi radi záchranné akcie v tejto oblasti. Posádky sa môžu dostať do kritických situácií, ktoré sú veľmi ťažko riešiteľné. Počas jedného leteckého výcviku, bolo to 17. augusta 1983, málo chýbalo, aby sa stala nehoda. Záchranári v masíve Kriváňa nacvičovali čo najrýchlejšie opustenie vrtuľníka v exponovanom teréne. Kriváň je pre pilotov nebezpečný štít. Každú sekundu musia byť v strehu, lebo na západnej strane Tatier, kde stojí tento ospevovaný štít, vejú zradné vetry zo všetkých strán, na helikoptéry tu číha termika i vzdušné diery.
+        Teda, v ten pekný jasný deň posádka vrtuľníka Mil Mi-8 pri vypúšťaní záchranára Martina Mladoňa s pomocou palubného navijaka na vrchol Kriváňa sa stroj znenazdajky, bez varovania, prepadol do doliny. Martin už bol na svojom stanovišti, stál na zemi, ešte sa nestihol odopnúť od lanka, keď prudký nečakaný pád vrtuľníka ho vytrhol aj s lankom do priepasti. Pilot zareagoval, odklonil stroj, natočil ho a pomaly ustálil. Len domyslieť si všetci mohli aké kruté chvíle pod vrtuľníkom prežíval Martin. Na tenkom lanku letel raz na jednu a potom na druhú stranu, ako na obrovskej hojdačke. Bol to okamih, na ktorý nikdy nezabudne. Ani potom, keď piloti lietali na nových moderných strojoch nevyhli sa nebezpečným nástrahám, ktoré pre nich pripravoval Kriváň. Prepadávali sa vo vzdušných dierach, prekonávali nebezpečné prúdenie vzduchu s nebývalým rešpektom lietajú okolo jeho stien aj teraz.[17]
+    </p>
+</div>
 @endsection
